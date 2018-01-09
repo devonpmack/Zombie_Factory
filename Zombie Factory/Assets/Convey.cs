a5aa5096eecd3d6f137b793b03fc9a32eb9a135a
STUDENT\S199811184 <devonpmack@gmail.com>
2018-01-09 11:25:20 -0500

    Camera drag + rotation of cubes

---
+++ Zombie Factory/Assets/Convey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Convey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector2 currentPos = transform.position;
+        transform.position = new Vector2(Mathf.Round(currentPos.x),
+                             Mathf.Round(currentPos.y));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        transform.Rotate(new Vector3(0,0,-90));
+    }
+}
