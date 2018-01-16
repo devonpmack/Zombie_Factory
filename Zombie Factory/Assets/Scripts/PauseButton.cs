e739920dd2e803d9525031dc0861e0548e821968
STUDENT\S199811184 <devonpmack@gmail.com>
2018-01-16 10:03:09 -0500

    Fixed shop scaling, improved UI, added brain count framework

---
+++ Zombie Factory/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour {
+
+    private bool running;
+    // Use this for initialization
+    void Start() {
+
+        running = true;
+
+    }
+
+    public void clicked()
+    {
+
+        running = !running;
+
+        if (running)
+        {
+            Time.timeScale = 1;
+           // GetComponentInChildren(Text).
+        }
+
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
